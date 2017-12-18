7886abafece926733c581120521731d22f01663d
Misha Plot <mishaplot@mail.ru>
2017-12-18 23:25:15 +0100

    add russian and english language

---
+++ PHP/inc/additional-languages/english/Jokes_by_BlueTeck.php
@@ -0,0 +1,38 @@
+<?php
+//define(_jokes_add_select_field, '<option value="[id]">[clantag] - [gegner]</option>');
+//define(_jokes_edit_select_field, '<option value="[id]" [sel]>[clantag] - [gegner]</option>');
+define(_config_jokes, 'Jokes');
+define(_jokes_unbearbeitet, 'unfinished');
+define(_jokes_geplant, 'planned');
+define(_jokes_archiv, 'archives');
+define(_joke_add,'Add joke');
+define(_jokes,'Jokes');
+define(_jokes_show_link, '<a href="../jokes/?action=show&amp;id=[id]" [hover]>[titel]</a>');
+define(_jokes_more,'read more');
+define(_jokes_show,'show');
+define(_jokes_empty, 'No joke for today entered');
+define(_jokes_eingereicht, 'Joke submitted by');
+define(_jokes_voted,'<i>You have already voted</i>');
+define(_jokes_login,'<i>Log in to use this function.</i>');
+define(_jokes_edit_link, 'editjokes&amp;id=[id]');
+define(_jokes_inaktiv, 'inactive');
+define(_jokes_thisdate, 'the following date:');
+define(_jokes_nextdate, 'next available date');
+define(_joke_edit,'Edit joke');
+define(_joke_edited,'edited joke');
+define(_jokes_date_forgiven,'For this date is already entered a joke');
+define(_joke_added,'joke added');
+define(_joke_deleted,'joke deleted');
+define(_confirm_del_joke,'Joke really delete?');
+define(_jokes_msg_title, 'New Joke submitted');
+define(_jokes_msg, 'Someone has submitted a Joke!<br /><br />
+<span class="fontBold">Nick:</span> [nick]<br />
+<span class="fontBold">Title:</span> [title]<br /><br />
+<span class="fontUnder"><span class="fontBold">Witz:</span></span><br />[content]<br /><br />
+<span class="fontBold"><a href="../admin/?admin=jokes&do=edit&id=[id]">Click here to edit the joke / unlock</a></span>');
+define(_joke_wait,'Your Joke has been submitted, it must now be unlocked only by an admin.');
+define(_perm_jokes,'Jokes manage');
+define(_jokes_top,'Here you see the top 10 jokes, get the best reviews of you.');
+define(_jokes_flop,'Here you see the flop 10 jokes that have received the worst reviews of you..');
+define(_joke_insert, 'Submit Joke');
+?>
---
+++ PHP/inc/additional-languages/russian/Jokes_by_BlueTeck.php
@@ -0,0 +1,39 @@
+﻿<?php
+//define(_jokes_add_select_field, '<option value="[id]">[clantag] - [gegner]</option>');
+//define(_jokes_edit_select_field, '<option value="[id]" [sel]>[clantag] - [gegner]</option>');
+define(_config_jokes, 'Анекдот(ы)');
+define(_jokes_unbearbeitet, 'необработанный');
+define(_jokes_geplant, 'запланированный');
+define(_jokes_archiv, 'архив');
+define(_joke_add,'Добавить Анекдот');
+define(_jokes,'Анекдотов');
+define(_jokes_show_link, '<a href="../jokes/?action=show&amp;id=[id]" [hover]>[titel]</a>');
+define(_jokes_more,'подробнее');
+define(_jokes_show,'показать');
+define(_jokes_empty, 'Анекдот(ы) на сегодняшний день не зарегистрирован(ы)');
+define(_jokes_eingereicht, 'Анекдот представел');
+define(_jokes_voted,'<i>Вы уже проголосовали</i>');
+define(_jokes_login,'<i>Login - чтобы использовать эту функцию.</i>');
+define(_jokes_edit_link, 'editjokes&amp;id=[id]');
+define(_jokes_inaktiv, 'неактивный');
+define(_jokes_thisdate, 'желаемая дата');
+define(_jokes_nextdate, 'cледующая возможная дата');
+define(_joke_edit,'Редактировать Анекдот');
+define(_joke_edited,'Анекдот отредактирован');
+define(_jokes_date_forgiven,'На эту дату уже вступил Анекдот (Пожалуйста, выберите другую дату)');
+define(_joke_added,'Анекдот добавил');
+define(_joke_deleted,'Анекдот удален');
+define(_confirm_del_joke,'Анекдот удалить?');
+define(_jokes_msg_title, 'Кто-то подал Новой Анекдот');
+define(_jokes_msg, '
+Кто-то подал Новой Анекдот!<br /><br />
+<span class="fontBold">Ник:</span> [nick]<br />
+<span class="fontBold">Название:</span> [title]<br /><br />
+<span class="fontUnder"><span class="fontBold">Анекдот:</span></span><br />[content]<br /><br />
+<span class="fontBold"><a href="../admin/?admin=jokes&do=edit&id=[id]">Для редактирования Анекдота / разблокировать, нажмите ->здесь<- </a></span>');
+define(_joke_wait,'Ваш Анекдот были представлены, он должен теперь разблокирован администратором.');
+define(_perm_jokes,'Анекдоты');
+define(_jokes_top,'Здесь вы можете увидеть 10 лучших анекдотов, которые получили большинство звезд.');
+define(_jokes_flop,'Здесь вы можете увидеть 10 худших анекдотов, которые получили худшие оценки.');
+define(_joke_insert, 'Отправить анекдот');
+?>
